c45ca1d28e928737744c4f8b2d3ca841ea47d7f0
zipated <zhuo392592689@live.com>
2023-11-11 10:50:34 +0800

    添加插件更新（未测试）

--- apps/update.js
+++ apps/update.js
@@ -2,6 +2,7 @@ import plugin from '../../../lib/plugins/plugin.js'
 import { createRequire } from 'module'
 import _ from 'lodash'
 import fs from 'fs'
+import { Restart } from '../../other/restart.js'
 
 const _path = process.cwd()
 const require = createRequire(import.meta.url)
@@ -15,6 +16,9 @@ const checkAuth = async function (e) {
   return true
 }
 
+// 是否在更新中
+let uping = false
+
 export class Update extends plugin {
   constructor () {
     super({
@@ -26,6 +30,11 @@ export class Update extends plugin {
           reg: `^#*(崩坏3|bh3|崩3|崩崩崩)?(强制)?(更新乐土攻略|乐土攻略更新)(ghproxy)?$`,  //留个强制好了（
           fnc: 'updateRes',
           desc: '【#管理】更新素材'
+        },
+        {
+          reg: `^#*(崩坏3|bh3|崩3|崩崩崩)?(强制)?(更新乐土攻略插件|乐土攻略插件(强制)?更新)(ghproxy)?$`,  //这么写能行吗（
+          fnc: 'update',
+          desc: '【#管理】插件更新'
         }
       ]
     })
@@ -77,6 +86,149 @@ export class Update extends plugin {
       })
     }
     return true
+  }
+
+  /**
+   * 异步执行git相关命令
+   * @param {string} cmd git命令
+   * @returns
+   */
+  async execSync (cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
+        resolve({ error, stdout, stderr })
+      })
+    })
+  }
+
+  /**
+   * 检查git是否安装
+   * @returns
+   */
+  async checkGit () {
+    let ret = await execSync('git --version', { encoding: 'utf-8' })
+    if (!ret || !ret.includes('git version')) {
+      await this.reply('请先安装git')
+      return false
+    }
+    return true
+  }
+
+  async update (e) {
+    if (!this.e.isMaster) return false
+
+    /** 检查是否正在更新中 */
+    if (uping) {
+      await this.reply('已有命令更新中..请勿重复操作')
+      return
+    }
+
+    /** 检查git安装 */
+    if (!(await this.checkGit())) return
+
+    const isForce = this.e.msg.includes('强制')
+
+    /** 执行更新 */
+    await this.runUpdate(isForce)
+
+    /** 是否需要重启 */
+    if (this.isUp) {
+      // await this.reply("更新完毕，请重启云崽后生效")
+      setTimeout(() => this.restart(), 2000)
+    }
+  }
+  
+  restart () {
+    new Restart(this.e).restart()
+  }
+
+  /**
+   * 星铁插件更新函数
+   * @param {boolean} isForce 是否为强制更新
+   * @returns
+   */
+  async runUpdate (isForce) {
+    const _path = './plugins/Elysian-Realm-plugin/'
+    let command = `git -C ${_path} pull --no-rebase`
+    if (isForce) {
+      command = `git -C ${_path} fetch --all && git -C ${_path} reset --hard HEAD`
+      this.e.reply('正在执行强制更新操作，请稍等')
+    } else {
+      this.e.reply('正在执行更新操作，请稍等')
+    }
+    /** 获取上次提交的commitId，用于获取日志时判断新增的更新日志 */
+    this.oldCommitId = await this.getcommitId('Elysian-Realm-plugin')
+    uping = true
+    let ret = await this.execSync(command)
+    uping = false
+
+    if (ret.error) {
+      logger.mark(`${this.e.logFnc} 更新失败：Elysian-Realm-plugin`)
+      this.gitErr(ret.error, ret.stdout)
+      return false
+    }
+
+    /** 获取插件提交的最新时间 */
+    let time = await this.getTime('Elysian-Realm-plugin')
+
+    if (/(Already up[ -]to[ -]date|已经是最新的)/.test(ret.stdout)) {
+      await this.reply(`Elysian-Realm-plugin已经是最新版本\n最后更新时间：${time}`)
+    } else {
+      await this.reply(`Elysian-Realm-plugin\n最后更新时间：${time}`)
+      this.isUp = true
+      /** 获取星铁组件的更新日志 */
+      let log = await this.getLog('Elysian-Realm-plugin')
+      await this.reply(log)
+    }
+
+    logger.mark(`${this.e.logFnc} 最后更新时间：${time}`)
+
+    return true
+  }
+
+  /**
+   * 处理更新失败的相关函数
+   * @param {string} err
+   * @param {string} stdout
+   * @returns
+   */
+  async gitErr (err, stdout) {
+    let msg = '更新失败！'
+    let errMsg = err.toString()
+    stdout = stdout.toString()
+
+    if (errMsg.includes('Timed out')) {
+      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
+      await this.reply(msg + `\n连接超时：${remote}`)
+      return
+    }
+
+    if (/Failed to connect|unable to access/g.test(errMsg)) {
+      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
+      await this.reply(msg + `\n连接失败：${remote}`)
+      return
+    }
+
+    if (errMsg.includes('be overwritten by merge')) {
+      await this.reply(
+        msg +
+        `存在冲突：\n${errMsg}\n` +
+        '请解决冲突后再更新，或者执行#强制更新，放弃本地修改'
+      )
+      return
+    }
+
+    if (stdout.includes('CONFLICT')) {
+      await this.reply([
+        msg + '存在冲突\n',
+        errMsg,
+        stdout,
+        '\n请解决冲突后再更新，或者执行#强制更新，放弃本地修改'
+      ])
+      return
+    }
+
+    await this.reply([errMsg, stdout])
   }
 
   /**
